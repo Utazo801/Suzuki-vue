8e00bcadff2d22091502487d4743e7581366c023
nitslaszlo <utazo.bence@gmail.com>
2021-01-06 17:43:07 +0100

    címek a slideoknak

--- SuzukiFrontend/src/components/AutoSlideComp.vue
+++ SuzukiFrontend/src/components/AutoSlideComp.vue
@@ -2,14 +2,22 @@
   <div>
     <b-carousel id="carousel" indicators controls img-width="1020" img-height="480">
       <b-carousel-slide
+        caption="Suzuki Ignis"
         img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/2018_Suzuki_Ignis_SZ5_SHVS_Allgrip_1.2_Front.jpg/1920px-2018_Suzuki_Ignis_SZ5_SHVS_Allgrip_1.2_Front.jpg"
       ></b-carousel-slide>
       <b-carousel-slide
+        caption="Suzuki SX4 S-Cross"
         img-src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/2018_Suzuki_SX4_S-Cross_SZ5_DDIS_Allgrip_1.6_Front.jpg/300px-2018_Suzuki_SX4_S-Cross_SZ5_DDIS_Allgrip_1.6_Front.jpg"
       ></b-carousel-slide>
-      <b-carousel-slide img-src="https://auto.suzuki.hu/swiftsport/assets/img/asvanyszurke@2x.webp"></b-carousel-slide>
-      <b-carousel-slide img-src="https://ipravda.sk/res/2020/07/15/thumbs/suzuki-swift-2021_04-nestandard1.jpg"></b-carousel-slide>
-      <b-carousel-slide img-src="https://upload.wikimedia.org/wikipedia/commons/f/fd/2016_Suzuki_Vitara_SZ5_Rugged_1.6.jpg"></b-carousel-slide>
+      <b-carousel-slide caption="Suzuki Swift Sport" img-src="https://auto.suzuki.hu/swiftsport/assets/img/asvanyszurke@2x.webp"></b-carousel-slide>
+      <b-carousel-slide
+        caption="Suzuki Swift"
+        img-src="https://ipravda.sk/res/2020/07/15/thumbs/suzuki-swift-2021_04-nestandard1.jpg"
+      ></b-carousel-slide>
+      <b-carousel-slide
+        caption="Suzuki Vitara"
+        img-src="https://upload.wikimedia.org/wikipedia/commons/f/fd/2016_Suzuki_Vitara_SZ5_Rugged_1.6.jpg"
+      ></b-carousel-slide>
     </b-carousel>
   </div>
 </template>
